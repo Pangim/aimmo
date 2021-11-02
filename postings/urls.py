064a7295bd4fa339900d56dcae44bbd758543cd1
leejunga <wjddk2940@gmail.com>
2021-11-02 16:39:42 +0900

    [ADD]: postings.views, unit test

--- postings/urls.py
+++ postings/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
+from postings.views import CommentView
+
 urlpatterns =[
+    path('/comments/<int:posting_id>', CommentView.as_view()),
+    path('/comments', CommentView.as_view())
 ]
